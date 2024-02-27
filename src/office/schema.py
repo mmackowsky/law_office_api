ebd252e9a2f4ca2835b1757c7e4d9311b3a605f5
mmackowsky <maciostah@gmail.com>
2024-02-27 18:13:14 +0100

    feat(schema): add Query class for resolvers

--- src/office/schema.py
+++ src/office/schema.py
@@ -132,3 +132,34 @@ class DeleteClient(graphene.Mutation):
         return DeleteClient(success=True)
 
 
+class Query(graphene.ObjectType):
+    lawyers = graphene.List(LawyerType)
+    lawyer = graphene.Field(LawyerType, id=graphene.Int())
+    clients = graphene.List(ClientType)
+    client = graphene.Field(ClientType, id=graphene.Int())
+    cases = graphene.List(CaseType)
+    case = graphene.Field(CaseType, id=graphene.Int())
+    specialities = graphene.List(SpecialityType)
+
+    def resolve_lawyers(self, info):
+        return Lawyer.objects.all()
+
+    def resolve_lawyer(self, info, id):
+        return Lawyer.objects.get(pk=id)
+
+    def resolve_clients(self, info):
+        return Client.objects.all()
+
+    def resolve_client(self, info, id):
+        return Client.objects.get(pk=id)
+
+    def resolve_cases(self, info):
+        return Case.objects.all()
+
+    def resolve_case(self, info, id):
+        return Case.objects.get(pk=id)
+
+    def resolve_specialities(self, info):
+        return Speciality.objects.all()
+
+
