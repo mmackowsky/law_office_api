ee535764b94fe3f5d3c0019a9003a08121a31a41
mmackowsky <maciostah@gmail.com>
2024-02-29 17:38:21 +0100

    feat(schema): update

--- src/core/schema.py
+++ src/core/schema.py
@@ -1,18 +1,10 @@
 import graphene
-from graphql_auth import mutations
-from graphql_auth.schema import UserQuery
 
 import office.schema
 
 
 class Query(office.schema.Query, graphene.ObjectType):
     pass
-
-
-class AuthMutation(graphene.ObjectType):
-    register = mutations.Register.Field()
-    verify_account = mutations.VerifyAccount.Field()
-    token_auth = mutations.ObtainJSONWebToken.Field()
 
 
 class Mutation(office.schema.Mutation, graphene.ObjectType):
