974c0b3a57ea4eca6c039fe65b99c0096bc489c6
mmackowsky <maciostah@gmail.com>
2024-03-01 18:43:06 +0100

    feat(schema): refactor file

--- src/office/schema.py
+++ src/office/schema.py
@@ -1,19 +1,9 @@
 import graphene
 from graphene_django import DjangoObjectType
 
+from .client_schema import ClientType, CreateClient, DeleteClient, UpdateClient
+from .lawyer_schema import CreateLawyer, DeleteLawyer, LawyerType, UpdateLawyer
 from .models import Case, Client, Lawyer, Speciality
-
-
-class LawyerType(DjangoObjectType):
-    class Meta:
-        model = Lawyer
-        fields = "__all__"
-
-
-class ClientType(DjangoObjectType):
-    class Meta:
-        model = Client
-        fields = "__all__"
 
 
 class CaseType(DjangoObjectType):
@@ -26,112 +16,6 @@ class SpecialityType(DjangoObjectType):
     class Meta:
         model = Speciality
         fields = "__all__"
-
-
-# Lawyer model mutations.
-class CreateLawyer(graphene.Mutation):
-    class Arguments:
-        first_name = graphene.String(required=True)
-        surname = graphene.String(required=True)
-        speciality = graphene.String(required=True)
-
-    lawyer = graphene.Field(LawyerType)
-
-    def mutate(self, info, first_name, surname, speciality):
-        lawyer = Lawyer(first_name=first_name, surname=surname, speciality=speciality)
-        lawyer.save()
-        return CreateLawyer(lawyer=lawyer)
-
-
-class UpdateLawyer(graphene.Mutation):
-    class Arguments:
-        speciality = graphene.String(required=True)
-
-    lawyer = graphene.Field(LawyerType)
-
-    def mutate(self, info, id, speciality=None):
-        try:
-            lawyer = Lawyer.objects.get(pk=id)
-        except Lawyer.DoesNotExist:
-            raise Exception("Lawyer does not exist")
-
-        if speciality is not None:
-            lawyer.speciality = speciality
-
-        lawyer.save()
-        return UpdateLawyer(lawyer=lawyer)
-
-
-class DeleteLawyer(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID(required=True)
-
-    success = graphene.Boolean()
-
-    def mutate(self, info, id):
-        try:
-            lawyer = Lawyer.objects.get(pk=id)
-        except Lawyer.DoesNotExist:
-            raise Exception("Lawyer is not exist")
-
-        lawyer.delete()
-        return DeleteLawyer(success=True)
-
-
-# Client model mutations.
-class CreateClient(graphene.Mutation):
-    class Arguments:
-        first_name = graphene.String(required=True)
-        surname = graphene.String(required=True)
-        lawyer = graphene.ID(required=True)
-        case = graphene.ID(required=True)
-
-    client = graphene.Field(ClientType)
-
-    def mutate(self, info, first_name, surname, lawyer, case):
-        lawyer = Lawyer.objects.get(pk=lawyer)
-        case = Case.objects.get(pk=case)
-        client = Client(
-            first_name=first_name, surname=surname, lawyer=lawyer, case=case
-        )
-        client.save()
-        return CreateClient(client=client)
-
-
-class UpdateClient(graphene.Mutation):
-    class Arguments:
-        lawyer = graphene.ID(required=True)
-
-    client = graphene.Field(ClientType)
-
-    def mutate(self, info, id, lawyer=None):
-        try:
-            lawyer = Lawyer.objects.get(pk=lawyer)
-            client = Client.objects.get(pk=id)
-        except Client.DoesNotExist or Lawyer.DoesNotExist:
-            raise Exception("Objects does not exists.")
-
-        if lawyer is not None:
-            client.lawyer.id = lawyer
-
-        client.save()
-        return UpdateClient(client=client)
-
-
-class DeleteClient(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID(required=True)
-
-    success = graphene.Boolean()
-
-    def mutate(self, info, id):
-        try:
-            client = Client.objects.get(pk=id)
-        except Client.DoesNotExist:
-            raise Exception("Clients does not exists.")
-
-        client.delete()
-        return DeleteClient(success=True)
 
 
 class Query(graphene.ObjectType):
